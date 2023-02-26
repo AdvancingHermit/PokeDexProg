0ade3d1a7f0a71a3db9370397e5ec888a8e426c7
AdvancingHermit <christiannkya@outlook.com>
2023-02-26 18:35:11 +0100

    DET ER SMUKT

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -5,7 +5,6 @@ import { PokemonClient } from 'pokenode-ts';
 let pokemonArray: string[] = [];
 let pokeTestArray: IPokemon[] = [];
 
-
 (async () => {
 
   for (let i = 1; i < 151; i++) {
@@ -34,25 +33,11 @@ function generatePokemon(): IPokemon[] {
 export class SearchComponent implements OnInit, OnDestroy {
 
   searchTerm = '';
-  term = '';
-
   error: string = "";
   sub: Subscription | undefined;
   pokeArr: IPokemon[] = generatePokemon();
   pokeTestSearch: IPokemon[] = [];
   testSprite!: pokeSprites;
-  url: string = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/";
-
-  clickFunction() {
-
-    alert(pokeTestArray.length + " This is The other one " + this.pokeArr.length);
-
-    this.convertSprites();
-
-  }
-  getImage(id: number): string{
-    return this.url + id + ".png";
-  }
 
   async ngOnInit(): Promise<void> {
 
@@ -62,18 +47,6 @@ export class SearchComponent implements OnInit, OnDestroy {
     }
     console.log("I AM DONE FIRST TEST");
   }
-
-  convertSprites() {
-    for (let i = 0; i < pokeTestArray.length; i++) {
-
-      this.testSprite = JSON.parse(JSON.stringify(pokeTestArray[i].sprites));
-      pokeTestArray[i].sprites = this.testSprite;
-      console.log(this.testSprite.front_default);
-
-    }
-  }
-
-
 
   async ngOnDestroy(): Promise<void> {
     this.sub?.unsubscribe();
