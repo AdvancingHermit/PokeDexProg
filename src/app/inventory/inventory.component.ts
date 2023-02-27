ddede1970628de61afa51b5c929a9f95f32b159f
lolOzzi <47964440+lolOzzi@users.noreply.github.com>
2023-02-27 22:38:08 +0100

    done

--- src/app/inventory/inventory.component.ts
+++ src/app/inventory/inventory.component.ts
@@ -1,30 +1,8 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { IPokemon } from '../fetchdata/api';
-import { generate, Subscription } from 'rxjs';
-import { PokemonClient, Pokemon } from 'pokenode-ts';
-import { getSafePropertyAccessString } from '@angular/compiler';
 import { DataService } from "../data/data.service";
 import {  Subscription } from 'rxjs';
 import {Pokemon } from 'pokenode-ts';
-
-
-// array of pokemon ids 1 to 100 for testing
-let ownedIDs: number[] = [].constructor(100).fill(0).map((x: number, y: number) => y + 1);
-
-
-async function getPokemon() {
-  let pokemonArray: Pokemon[] = [];
-  for (let i = 0; i < ownedIDs.length; i++) {
-    const api = new PokemonClient();
-
-    await api
-      .getPokemonById(ownedIDs[i])
-      .then((data) => {pokemonArray[i] = (data)}) 
-      .catch((error) => console.error(error));
-  }
-  console.log(pokemonArray);
-  return pokemonArray;
-}
 
 @Component({
   selector: 'app-inventory',
@@ -38,9 +16,7 @@ export class InventoryComponent implements OnInit, OnDestroy {
   sub: Subscription | undefined;
   pokeArr: Pokemon[] = [];
   stats: string[][] = [[]];
-
   ownedIDs: number[] = [];
-
   
   constructor(private data: DataService) { }
 
